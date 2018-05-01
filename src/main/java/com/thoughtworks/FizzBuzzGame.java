dbf7314c90cae3432db0bb67cba2b0876bfca37f
llr392425595 <392425595@qq.com>
2018-05-01 13:26:37 +0800

    重构

--- src/main/java/com/thoughtworks/FizzBuzzGame.java
+++ src/main/java/com/thoughtworks/FizzBuzzGame.java
@@ -6,13 +6,45 @@ import java.util.List;
 
 public class FizzBuzzGame {
 
+  private static final String WHIZZ = "Whizz";
+  private static final String BUZZ = "Buzz";
+  private static final String FIZZ = "Fizz";
+  private static final String NO_STR = "";
+  private static final int FIZZ_NUM = 3;
+  private static final int BUZZ_NUM = 5;
+  private static final int WHIZZ_NUM = 7;
   private List<String> results = new ArrayList<>();
 
   public void start(int count) {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
     for (int i = 1; i <= count; i++) {
-      results.add(fizzBuzzConverter.transform(i));
+      results.add(transform(i));
     }
+  }
+
+  public String transform(int number) {
+    String word = NO_STR;
+    String num = String.valueOf(number);
+    if (num.contains(String.valueOf(FIZZ_NUM))) {
+      return FIZZ;
+    }
+    word += threeMultiple(number) + fiveMultiple(number) + sevenMultiple(number);
+    if (word.equals("")) {
+      return num;
+    }
+    return word;
+
+  }
+
+  private String sevenMultiple(int number) {
+    return number % WHIZZ_NUM == 0 ? WHIZZ : NO_STR;
+  }
+
+  private String fiveMultiple(int number) {
+    return number % BUZZ_NUM == 0 ? BUZZ : NO_STR;
+  }
+
+  private String threeMultiple(int number) {
+    return number % FIZZ_NUM == 0 ? FIZZ : NO_STR;
   }
 
   public List<String> getResults() {
--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -2,20 +2,27 @@ package com.thoughtworks;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzGameTest {
 
+  private FizzBuzzGame game;
+
+  @Before
+  public void setup() {
+    game = new FizzBuzzGame();
+  }
+
   @Test
   public void should_return_Fizz_when_have_3() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(13);
+    String value1 = game.transform(13);
     String expected1 = "Fizz";
-    String value2 = fizzBuzzConverter.transform(35);
+    String value2 = game.transform(35);
     String expected2 = "Fizz";
-    String value3 = fizzBuzzConverter.transform(43);
+    String value3 = game.transform(43);
     String expected3 = "Fizz";
-    String value4 = fizzBuzzConverter.transform(53);
+    String value4 = game.transform(53);
     String expected4 = "Fizz";
 
     assertEquals(expected1, value1);
@@ -27,8 +34,7 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_numStr_when_given_1() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value = fizzBuzzConverter.transform(1);
+    String value = game.transform(1);
     String expected = "1";
 
     assertEquals(expected, value);
@@ -36,10 +42,9 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_Fizz_when_number_is_multiple_of_3() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(3);
-    String value2 = fizzBuzzConverter.transform(6);
-    String value3 = fizzBuzzConverter.transform(9);
+    String value1 = game.transform(3);
+    String value2 = game.transform(6);
+    String value3 = game.transform(9);
     String expected = "Fizz";
 
     assertEquals(expected, value1);
@@ -49,10 +54,9 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_Buzz_when_number_is_multiple_of_5() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(5);
-    String value2 = fizzBuzzConverter.transform(10);
-    String value3 = fizzBuzzConverter.transform(20);
+    String value1 = game.transform(5);
+    String value2 = game.transform(10);
+    String value3 = game.transform(20);
     String expected = "Buzz";
 
     assertEquals(expected, value1);
@@ -62,10 +66,9 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_Whizz_when_number_is_multiple_of_7() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(7);
-    String value2 = fizzBuzzConverter.transform(14);
-    String value3 = fizzBuzzConverter.transform(28);
+    String value1 = game.transform(7);
+    String value2 = game.transform(14);
+    String value3 = game.transform(28);
     String expected = "Whizz";
 
     assertEquals(expected, value1);
@@ -75,9 +78,8 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_FizzBuzz_when_number_is_multiple_of_3_5() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(15);
-    String value2 = fizzBuzzConverter.transform(45);
+    String value1 = game.transform(15);
+    String value2 = game.transform(45);
     String expected = "FizzBuzz";
 
     assertEquals(expected, value1);
@@ -86,9 +88,8 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_FizzWhizz_when_number_is_multiple_of_3_7() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(21);
-    String value2 = fizzBuzzConverter.transform(42);
+    String value1 = game.transform(21);
+    String value2 = game.transform(42);
     String expected = "FizzWhizz";
 
     assertEquals(expected, value1);
@@ -97,9 +98,8 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_BuzzWhizz_when_number_is_multiple_of_5_7() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(70);
-    String value2 = fizzBuzzConverter.transform(140);
+    String value1 = game.transform(70);
+    String value2 = game.transform(140);
     String expected = "BuzzWhizz";
 
     assertEquals(expected, value1);
@@ -108,9 +108,8 @@ public class FizzBuzzGameTest {
 
   @Test
   public void should_return_FizzBuzzWhizz_when_number_is_multiple_of_3_5_7() {
-    FizzBuzzConverter fizzBuzzConverter = new FizzBuzzConverter();
-    String value1 = fizzBuzzConverter.transform(105);
-    String value2 = fizzBuzzConverter.transform(210);
+    String value1 = game.transform(105);
+    String value2 = game.transform(210);
     String expected = "FizzBuzzWhizz";
 
     assertEquals(expected, value1);
